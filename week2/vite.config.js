f64ee2c69b9a584254d19a8c422e96f16e986fcf
ella.fang <ella.fang@gmai.com>
2026-02-22 20:16:50 +0800

    Updates

--- week2/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // 必須與你的 Repo 名稱一致，前後都要有斜線
-  base: '/React-Week2/', 
-})
