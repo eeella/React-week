fbf54fea5728d64120a4a8165ebc35865cc40d70
ella.fang <ella.fang@gmai.com>
2026-02-22 19:29:41 +0800

    for wee2

--- week2/vite.config.js
+++ week2/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-week/', 
+  // 必須與你的 Repo 名稱一致，前後都要有斜線
+  base: '/React-Week2/', 
 })
